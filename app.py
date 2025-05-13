09b2480d26a30d0836dd2284e981084085b27c1f
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-05-13 15:25:59 +0530

    Final

--- app.py
+++ app.py
@@ -308,4 +308,4 @@ def get_translations(lang):
     return {key: val.get(lang, val["en"]) for key, val in translations.items()}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
