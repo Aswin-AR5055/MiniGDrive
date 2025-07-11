d6aff01eeaa7b34ec172cb389ecde61452620c8d
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-07-11 12:10:42 +0530

    creating new instance

--- Main.tf
+++ Main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "my_instance" {
     ami = data.aws_ami.my_ubuntu_ami.id
     instance_type = "t2.micro"
     key_name = "newubuntukeypair"
-    vpc_security_group_ids = [aws_security_group.my_sg_new.id]
+    vpc_security_group_ids = [aws_security_group.my_sg_new_2.id]
     tags = {
     Name = "ERDiagramGeneratorserver"
     }
