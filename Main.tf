3d7b7d8aca1d2bfcaa662c54eaa607cf28d85069
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-09-22 18:50:45 +0530

    opened traffic for more ports

--- Main.tf
+++ Main.tf
@@ -61,6 +61,20 @@ resource "aws_security_group" "my_sg_group_ar" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 
+  ingress {
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress {
+    from_port = 8000
+    to_port = 8000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
