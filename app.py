7e61e51ab8ffac897629ee5f56910fd092b1c0d5
Aswin-AR5055 <aswinraj2003@gmail.com>
2025-09-30 21:10:27 +0530

    upgrade to s3 cloud storage process started

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from db_schema import get_db_connection, init_db
 init_db()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
